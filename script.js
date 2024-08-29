7cf373f1f1dc30035b6c73d4df2e6de59bad7330
Phillip Martinez <phillip.martinez210@gmail.com>
2024-08-29 13:31:54 -0500

    Added check to ensure that there is not already a quote showing.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const newQuoteButton = document.querySelector("#newQuoteButton");
 const clearButton = document.querySelector("#clearButton");
 const quoteDataContainer = document.querySelector("#quoteDataContainer");
 
-async function getQuoteDate() {
+async function getQuoteData() {
     const url = "https://api.quotable.io/random";
     try {
         const response = await fetch(url);
@@ -20,19 +20,22 @@ async function getQuoteDate() {
 // TODO - Add JS functionality to generate a new quote and clear existing one
 async function displayQuote() {
     try {
-        const quote = await getQuoteDate();
-        console.log(`Quote Author: ${quote.author}\nQuote: "${quote.content}"`);
-
-        const authorDiv = document.createElement("div");
-        authorDiv.classList.add("authorDiv");
-        authorDiv.innerHTML = `<strong>Quote Author:</strong> ${quote.author}`;
-        quoteDataContainer.appendChild(authorDiv);
-
-        const quoteDiv = document.createElement("div");
-        quoteDiv.classList.add("quoteDiv");
-        quoteDiv.innerHTML = `<strong>Quote: </strong>"${quote.content}"`
-        quoteDataContainer.appendChild(quoteDiv);
-
+        if (quoteDataContainer.innerHTML === "") {
+            const quote = await getQuoteData();
+            console.log(`Quote Author: ${quote.author}\nQuote: "${quote.content}"`);
+    
+            const authorDiv = document.createElement("div");
+            authorDiv.classList.add("authorDiv");
+            authorDiv.innerHTML = `<strong>Quote Author:</strong> ${quote.author}`;
+            quoteDataContainer.appendChild(authorDiv);
+    
+            const quoteDiv = document.createElement("div");
+            quoteDiv.classList.add("quoteDiv");
+            quoteDiv.innerHTML = `<strong>Quote: </strong>"${quote.content}"`
+            quoteDataContainer.appendChild(quoteDiv);
+        } else {
+            return;
+        }
     } catch (error) {
         console.error(error.message);
     }
